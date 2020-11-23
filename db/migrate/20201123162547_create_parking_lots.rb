3abb8b1ad1ab7574e1536e5c8e3b6290583007ca
olaf herzig <olaf.s.herzig@gmail.com>
2020-11-23 05:28:05 +0100

    recreating model parking lots

--- db/migrate/20201123162547_create_parking_lots.rb
+++ db/migrate/20201123162547_create_parking_lots.rb
@@ -1,10 +1,11 @@
 class CreateParkingLots < ActiveRecord::Migration[6.0]
   def change
     create_table :parking_lots do |t|
+      t.string :name
       t.string :address
       t.integer :price
       t.string :vehicle_type
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
