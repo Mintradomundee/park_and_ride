6b2fd2128f83199bdaa7cb7223fad57da94841c0
Mintra Domundee <mintra.mynt@gmail.com>
2020-11-24 23:43:50 +0700

    Add name to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_162547) do
+ActiveRecord::Schema.define(version: 2020_11_24_164307) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "parking_lots", force: :cascade do |t|
-    t.string "name"
     t.string "address"
     t.integer "price"
     t.string "vehicle_type"
@@ -23,17 +23,6 @@ ActiveRecord::Schema.define(version: 2020_11_23_162547) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_parking_lots_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "content"
-    t.integer "rating_star"
-    t.bigint "parking_lot_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["parking_lot_id"], name: "index_reviews_on_parking_lot_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,11 +33,10 @@ ActiveRecord::Schema.define(version: 2020_11_23_162547) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "parking_lots", "users"
-  add_foreign_key "reviews", "parking_lots"
-  add_foreign_key "reviews", "users"
 end
