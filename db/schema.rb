c69c91a0f4e322d0a729a789183394250412f76a
Mintra Domundee <mintra.mynt@gmail.com>
2020-11-25 20:49:26 +0700

    no change, just test

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_11_25_132608) do
   end
 
   create_table "parking_lots", force: :cascade do |t|
-    t.string "name"
     t.string "address"
     t.integer "price"
     t.string "vehicle_type"
@@ -62,17 +61,6 @@ ActiveRecord::Schema.define(version: 2020_11_25_132608) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_parking_lots_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "content"
-    t.integer "rating_star"
-    t.bigint "parking_lot_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["parking_lot_id"], name: "index_reviews_on_parking_lot_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -99,7 +87,5 @@ ActiveRecord::Schema.define(version: 2020_11_25_132608) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "parking_lots", "users"
-  add_foreign_key "reviews", "parking_lots"
-  add_foreign_key "reviews", "users"
   add_foreign_key "vehicles", "users"
 end
