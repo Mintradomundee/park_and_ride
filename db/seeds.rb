476ab642b5e03ddf218e8be4d702f6158a74d65a
tifana rosa <tifanarosa@gmail.com>
2020-11-25 14:41:25 +0100

    fix bugs on the cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
+# require 'faker'
 
-ParkingLot.destroy_all
-User.destroy_all
-puts "Parking lots and User destroyed"
+# ParkingLot.destroy_all
+# User.destroy_all
+# puts "Parking lots and User destroyed"
 
-adam = User.create(
-  email: "adam@adam.com",
-  password: "123456"
-)
+# adam = User.create(
+#   email: "adam@adam.com",
+#   password: "123456"
+# )
 
-10.times do
+# 10.times do
 
-  parking_lots = ParkingLot.new(
-    name: Faker::Name.name,
-    address: Faker::Address.street_address,
-    vehicle_type: Faker::Vehicle.drive_type,
-    price: Faker::Commerce.price,
-    user: adam
-  )
+#   parking_lots = ParkingLot.new(
+#     name: Faker::Name.name,
+#     address: Faker::Address.street_address,
+#     vehicle_type: Faker::Vehicle.drive_type,
+#     price: Faker::Commerce.price,
+#     user: adam
+#   )
 
-  parking_lots.save!
+#   parking_lots.save!
 
-end
-puts 'Finished!'
+# end
+# puts 'Finished!'
