38423f6c531536ee67d79c1ea3d26a22d89cab7b
Mintra Domundee <mintra.mynt@gmail.com>
2020-11-27 22:32:48 +0700

    add marker

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   def home
-
+    @parking_lots = ParkingLot.all
+    @markers = @parking_lots.geocoded.map do |parking_lot|
+      {
+        lat: parking_lot.latitude,
+        lng: parking_lot.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { parking_lot: parking_lot }),
+        image_url: helpers.asset_url('marker.png')
+      }
+    end
   end
 
   def confirmation
