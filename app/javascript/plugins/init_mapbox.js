613a4661445b6a32aac68f02617898f5937f5aad
Mintra Domundee <mintra.mynt@gmail.com>
2020-11-27 22:19:08 +0700

    fix position and styling

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -41,8 +41,8 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers); //markers
     fitMapToMarkers(map, markers);
-    // map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-    //  mapboxgl: mapboxgl }));
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl }));
   }
 };
 
