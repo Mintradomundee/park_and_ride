e1bbd53c440ba22948917f9161a668f861e14d63
Nikolas Maerker <nikolas.maerker@gmx.de>
2020-11-27 15:17:35 +0100

    map icons color

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,16 +39,74 @@ adam = User.create!(
   password: "123456"
 )
 
-30.times do
+parking_lots = ParkingLot.create!(
+  name: "Pergamonmuseum",
+  address: "Bodestraße 1-3, 10178 Berlin",
+  vehicle_type: "car",
+  price: 7,
+  user: adam
+)
 
-  parking_lots = ParkingLot.new(
-    name: Faker::Name.name,
-    address: Faker::Address.city,
-    vehicle_type: Faker::Vehicle.drive_type,
-    price: Faker::Commerce.price,
-    user: adam
-  )
+parking_lots = ParkingLot.create!(
+  name: "Institut für Zeitgenössische Kunst",
+  address: "Auguststraße 69, 10117 Berlin",
+  vehicle_type: "car",
+  price: 5,
+  user: adam
+)
 
-  parking_lots.save!
-end
+parking_lots = ParkingLot.create!(
+  name: "Brandenburger Tor",
+  address: "Pariser Platz, 10117 Berlin",
+  vehicle_type: "car",
+  price: 8,
+  user: adam
+)
+
+parking_lots = ParkingLot.create!(
+  name: "Tränenpalast",
+  address: "Reichstagufer 17, 10117 Berlin",
+  vehicle_type: "car",
+  price: 10,
+  user: adam
+)
+
+parking_lots = ParkingLot.create!(
+  name: "Reichstagsgebäude",
+  address: "Platz der Republik 1, 11011 Berlin",
+  vehicle_type: "car",
+  price: 12,
+  user: adam
+)
+
+parking_lots = ParkingLot.create!(
+  name: "Berliner Dom",
+  address: "Am Lustgarten, 10178 Berlin",
+  vehicle_type: "car",
+  price: 9,
+  user: adam
+)
+
+parking_lots = ParkingLot.create!(
+  name: "Le Wagon Parking",
+  address: "Rudi-Dutschke-Straße 26, 10969 Berlin",
+  vehicle_type: "car",
+  price: 2,
+  user: adam
+)
+
+
+
+# 30.times do
+
+#   parking_lots = ParkingLot.new(
+#     name: Faker::Name.name,
+#     address: Faker::Address.city,
+#     vehicle_type: Faker::Vehicle.drive_type,
+#     price: Faker::Commerce.price,
+#     user: adam
+#   )
+
+#   parking_lots.save!
+# end
 puts 'Finished!'
