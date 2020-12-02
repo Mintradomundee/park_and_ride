203f1d9098ba3301ad39c10cc445f4b8a932e94e
tifana rosa <tifanarosa@gmail.com>
2020-12-02 16:19:50 +0100

    finishing cards, photos, carousel

--- app/models/parking_lot.rb
+++ app/models/parking_lot.rb
@@ -5,7 +5,7 @@ class ParkingLot < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   monetize :price_cents
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   
   include PgSearch::Model
   pg_search_scope :global_search,
